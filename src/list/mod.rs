107e5f11d63faade38476e66ef733168aa55cff9
Arav Singhal <singhal.arav@gmail.com>
2019-04-09 20:07:51 -0500

    Fix next id bug on allocation, Update tests

--- src/list/mod.rs
+++ src/list/mod.rs
@@ -32,32 +32,33 @@ pub struct FreeList<T> {
 impl<T> FreeList<T> {
     pub fn new(initial_capacity: FreeListId) -> FreeList<T> {
         let mut node_pool = Vec::with_capacity(initial_capacity);
-        
+
         FreeList {
             object_pool: node_pool,
             object_count: 0,
             next_free_object_id: 0,
         }
     }
-    
+
     pub fn allocate(&mut self, data: T) -> FreeListId {
         assert!(self.next_free_object_id <= self.object_pool.len());
-        
+
         if self.next_free_object_id == self.object_pool.len() {
             // Expand our node pool
             self.object_pool.push(FreeListNode::new(data, self.next_free_object_id + 1));
+        } else {
+            self.object_pool[self.next_free_object_id].data = Some(data);
         }
-            else {
-                self.object_pool[self.next_free_object_id].data = Some(data);
-            }
-        
+
         self.object_count += 1;
-        
+
         let object_id = self.next_free_object_id;
-        self.next_free_object_id += 1;
+
+        self.next_free_object_id = self.object_pool[object_id].next;
+
         object_id
     }
-    
+
     pub fn free(&mut self, id: FreeListId) {
         let pool_object = self.object_pool.get_mut(id).expect("Invalid object id");
 
@@ -65,16 +66,16 @@ impl<T> FreeList<T> {
         pool_object.data = None;
 
         self.next_free_object_id = id;
-        
+
         self.object_count -= 1;
     }
-    
+
     pub fn get(&self, id: FreeListId) -> &T {
         self.object_pool.get(id)
             .and_then(|node| node.data.as_ref())
             .expect("Invalid object id")
     }
-    
+
     pub fn get_mut(&mut self, id: FreeListId) -> &mut T {
         self.object_pool.get_mut(id)
             .and_then(|node| node.data.as_mut())
--- src/list/tests.rs
+++ src/list/tests.rs
@@ -1,36 +1,48 @@
-#![cfg(test)]
-
 use super::*;
 
 #[test]
 fn allocate() {
     let mut pool = FreeList::default();
-    let id_a = pool.allocate(0);
-    {
-        let a = pool.get_mut(id_a);
-        *a = 32u32;
-    }
+    let a = pool.allocate(1u32);
+
     assert_eq!(pool.object_count, 1);
-    assert_eq!(pool.next_free_object_id, id_a + 1);
-    assert_eq!(pool.get(id_a), &32);
+    assert_eq!(pool.next_free_object_id, a + 1);
+
+    let b = pool.allocate(1);
+
+    assert_eq!(pool.object_count, 2);
+    assert_eq!(pool.next_free_object_id, b + 1);
+
+    let c = pool.allocate(1);
+
+    assert_eq!(pool.object_count, 3);
+    assert_eq!(pool.next_free_object_id, c + 1);
+
+
+    pool.free(b);
+
+    assert_eq!(pool.object_count, 2);
+    assert_eq!(pool.next_free_object_id, b);
+
+    let new_id = pool.allocate(1);
+
+    assert_eq!(new_id, b);
+    assert_eq!(pool.get(new_id), &1);
+    assert_eq!(pool.object_count, 3);
+    assert_eq!(pool.next_free_object_id, c + 1);
 }
 
 #[test]
 fn free() {
     let mut pool = FreeList::default();
     let (a, b, c) = (pool.allocate(0), pool.allocate(0), pool.allocate(0));
-    
+
     {
         *pool.get_mut(b) = 111u32;
     }
-    
+
     pool.free(b);
-    
+
     assert_eq!(pool.object_count, 2);
     assert_eq!(pool.next_free_object_id, b);
-    
-    let new_id = pool.allocate(0);
-    
-    assert_eq!(b, new_id);
-    assert_eq!(pool.get(new_id), &u32::default())
 }
